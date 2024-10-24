9a570f715d010b390ca38a707bcf58b9556dd1da
Theis Nielsen Haahr <Theishaahr@yahoo.dk>
2024-10-24 14:26:09 +0200

    Added files from previous project

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="toggleForm">{{ isLogin ? 'Go to Signup' : 'Go to Login' }}</button>
+    <LoginForm v-if="isLogin" />
+    <SignupForm v-else />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import SignupForm from "./../components/SignupForm.vue"
+import LoginForm from "./../components/LoginForm.vue"
+
+const isLogin = ref(true)
+
+
+const toggleForm = () => {
+    isLogin.value = !isLogin.value;
+};
+
+</script>
+
+<style lang="scss" scoped></style>
