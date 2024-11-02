97d373d371a44cb75b2750e108c85e62fd576f13
AnneRoe <10461971@ucn.dk>
2024-11-02 17:56:43 +0100

    orderlist added - confirmation page

--- src/components/OrderConfirmComp.vue
+++ src/components/OrderConfirmComp.vue
@@ -1,10 +1,20 @@
 <script setup>
+import useCart from "../Services/useCart";
+
+const { cart } = useCart();
 </script>
 
 <template>
     <div class="order-conf-container">
-        <p>Tak for dit køb hos TechTove!</p>
-        
+        <h3>Tak for dit køb hos TechTove!</h3>
+        <div>
+            <h4>Glæd dig til:</h4>
+            <div class="order-conf-items" v-for="item in cart" :key="item.id">
+                <p>{{ item.titel }}</p>
+                <p>{{ item.pris }} DKK</p>
+                <p>{{ item.quantity }} stk</p>
+            </div>
+        </div>
     </div>
 </template>
 
